ded23333ca3c63d10d0464556a2e85ed4d153e04
Yusuke Minei <e175703@ie.u-ryukyu.ac.jp>
2019-12-07 22:13:43 +0900

    歌詞を取ってくるようになった

--- get_lyrics.py
+++ get_lyrics.py
@@ -17,16 +17,36 @@ def get_titleinfos(groups):
     return titleinfos
 
 
+def get_lyrics(song_url):
+    req_song = requests.get(song_url)
+    song_html = req_song.content
+    song_soup = BeautifulSoup(song_html, "html5lib")
+    if re.search("iM@S Cover Data", str(song_soup)):
+        return None
+    lyrics_sec = song_soup.find(class_="wikitable")
+    rows = lyrics_sec.find_all("tr")
+    lyrics = ""
+    for row in rows:
+        cell = row.find('td')
+        if cell is not None:
+            lyrics += cell.text
+
+    return lyrics
+
+
 if __name__ == '__main__':
     route_url = "https://www.project-imas.com"
-    rr = requests.get(route_url + "/wiki/Category:Songs")
+    req_title = requests.get(route_url + "/wiki/Category:Songs")
     with open("titles.txt", 'r') as f:
         req_titles = f.readlines()
 
-    html = rr.content
-    soup = BeautifulSoup(html, "html5lib")
-    groups = soup.find_all(class_="mw-category-group")
+    title_html = req_title.content
+    title_soup = BeautifulSoup(title_html, "html5lib")
+    groups = title_soup.find_all(class_="mw-category-group")
     
     titleinfos = get_titleinfos(groups)
-    url = route_url + titleinfos[0]["url"]
-    print(url)
+    song_url = route_url + titleinfos[0]["url"]
+    print(song_url)
+    
+    lyrics = get_lyrics(song_url)
+    print(lyrics)
