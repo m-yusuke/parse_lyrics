26b11077a3cfa8b6d1e12b484dd3f0f928653c47
Yusuke Minei <e175703@ie.u-ryukyu.ac.jp>
2019-12-09 00:13:28 +0900

    曲名と作品, urlを取ってくるようにした。title取ってくるやつは不要になったので消した

--- get_lyrics.py
+++ get_lyrics.py
@@ -3,26 +3,18 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import pprint
 
 
-def get_titleinfos(groups):
-    titleinfos = []
-    for group in groups:
-        group_titles = group.find_all("li")
-        for group_title in group_titles:
-            a = group_title.find('a')
-            tmp = {"title": group_title.text, "url": a.attrs['href']}
-            titleinfos.append(tmp)
-
-    return titleinfos
-
-
-def get_lyrics(song_url):
+def get_lyrics(songinfo):
+    songattr = {
+            "title": "",
+            "song_title": "",
+            "lyrics": ""
+            }
     req_song = requests.get(song_url)
     song_html = req_song.content
     song_soup = BeautifulSoup(song_html, "html5lib")
-    if re.search("iM@S Cover Data", str(song_soup)):
-        return None
     lyrics_sec = song_soup.find(class_="wikitable")
     rows = lyrics_sec.find_all("tr")
     lyrics = ""
@@ -31,37 +23,64 @@ def get_lyrics(song_url):
         if cell is not None:
             lyrics += cell.text
 
-    return lyrics
+    songattr["lyrics"] = lyrics
+
+    return songattr
+
+
+def get_songinfos(url):
+    req_url = requests.get(url)
+    html = req_url.content
+    songinfos = {}
+    titles = [
+            "THE IDOLM@STER",
+            "THE IDOLM@STER CINDERELLA GIRLS",
+            "THE IDOLM@STER MILLION LIVE!",
+            "THE IDOLM@STER SideM",
+            "THE IDOLM@STER SHINYCOLORS"
+            ]
+    soup = BeautifulSoup(html, "html5lib")
+    tables = soup.find_all("table")
+
+    song_index = 0
+    for table in tables:
+        if re.search("(Famison|SideM|iDOLM@STER\.KR|Xenoglossia|Radio|Cover)", str(table)):
+            continue
+        elif re.search("Cinderella Girls", str(table)):
+            title = titles[1]
+        elif re.search("Million Live", str(table)):
+            title = titles[2]
+        elif re.search("Shiny Colors", str(table)):
+            title = titles[4]
+        else:
+            title = titles[0]
+
+        tbody = table.find("tbody")
+        rows = tbody.find_all('tr')
+        for row in rows:
+            songinfo = {
+                    "song_title": "",
+                    "title": title,
+                    "url": ""
+                    }
+            cells = row.find_all('td')
+            if len(cells) > 0 and re.search("page does not exist", str(cells)) is None:
+                songinfo["song_title"] = cells[1].text
+                songinfo["url"] = cells[1].find("a").attrs['href']
+                songinfos[song_index] = songinfo
+                song_index += 1
+
+    return songinfos
+
 
 
 if __name__ == '__main__':
     route_url = "https://www.project-imas.com"
-    titles_url = route_url + "/wiki/Category:Songs"
-    req_title = requests.get(titles_url)
-    with open("titles.txt", 'r') as f:
-        req_titles = f.readlines()
+    songlist_url = route_url + "/wiki/Official_Song_Listing"
 
-    titleinfos = []
-    while True:
-        print(titles_url)
-        title_html = req_title.content
-        title_soup = BeautifulSoup(title_html, "html5lib")
-        groups = title_soup.find_all(class_="mw-category-group")
+    songinfos = get_songinfos(songlist_url)
 
-        titleinfos += get_titleinfos(groups)
-
-        content = title_soup.find(id="mw-pages")
-        seeks = content.find_all("a", recursive=False)
-        if seeks[-1].text == "next page":
-            titles_url = route_url + seeks[-1].attrs['href']
-            req_title = requests.get(titles_url)
-        else:
-            break
+    pprint.pprint(songinfos)
     
-    for i in titleinfos:
-        print(f"title = {i['title']}, url = {i['url']}")
-    song_url = route_url + titleinfos[0]["url"]
-    #print(song_url)
-    
-    #lyrics = get_lyrics(song_url)
+    #lyrics = get_songinfo(song_url)
     #print(lyrics)
